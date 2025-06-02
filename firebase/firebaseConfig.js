0aac9bd760f9f7eb57ddd6eb47ca89aad4c8f154
unknown <alvaronoriegam@gmail.com>
2025-06-02 20:06:47 +0200

    firebase

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore"; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIjXavdxSTSjX9gKGNUiYKL6IaR0yKvnk",
+  authDomain: "pocubicacion-5752e.firebaseapp.com",
+  projectId: "pocubicacion-5752e",
+  storageBucket: "pocubicacion-5752e.firebasestorage.app",
+  messagingSenderId: "806413473076",
+  appId: "1:806413473076:web:3b6c3b2875c7a017598161",
+  measurementId: "G-BE84X41SZZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const analytics = getAnalytics(app);
---
+++ screens/HomeScreen.js
@@ -0,0 +1,85 @@
+// screens/HomeScreen.js
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import MapView, { Marker } from 'react-native-maps';
+import { saveUserLocation } from '../firebase/geo';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebaseConfig';
+
+export default function HomeScreen() {
+  const [location, setLocation] = useState(null);
+  const [usersNearby, setUsersNearby] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permiso de ubicación denegado');
+        return;
+      }
+
+      const currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation.coords);
+      console.log('guardando ubicacion')
+      saveUserLocation('user_123', currentLocation.coords);
+
+      const interval = setInterval(async () => {
+        const newLocation = await Location.getCurrentPositionAsync({});
+        setLocation(newLocation.coords);
+        console.log('Actualizando ubicación:', newLocation.coords);
+        await saveUserLocation('user_123', newLocation.coords);
+
+        const snapshot = await getDocs(collection(db, 'locations'));
+        const users = snapshot.docs.map(doc => doc.data());
+        setUsersNearby(users);
+        console.log('Usuarios cercanos cargados:', users.length);
+        }, 60000);
+
+      return () => clearInterval(interval);
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05
+          }}
+          showsUserLocation={true}
+        >
+          {usersNearby.map((user, index) => (
+            <Marker
+              key={index}
+              coordinate={{
+                latitude: user.latitude,
+                longitude: user.longitude
+              }}
+              title={`Usuario ${index + 1}`}
+            />
+          ))}
+        </MapView>
+      ) : (
+        <Text style={styles.loadingText}>Cargando ubicación...</Text>
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    flex: 1
+  },
+  loadingText: {
+    marginTop: 50,
+    textAlign: 'center'
+  }
+});
