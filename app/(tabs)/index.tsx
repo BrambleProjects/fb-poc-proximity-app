136b628783dc6117f4b44be0ecbe126d656448a0
bramble.team <bramble.team@gmail.com>
2025-06-04 18:07:36 +0000

    Prueba login screen  directa

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeScreen from '../../screens/HomeScreen';
+import LoginScreen from '../../screens/LoginScreen';
 
 export default function Index() {
-  return <HomeScreen />;
+  return <LoginScreen />;
 }
--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -21,3 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 const analytics = getAnalytics(app);
+
+export { app };
